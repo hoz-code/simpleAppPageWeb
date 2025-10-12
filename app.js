9751f785fb24dad034f83548636471789781d29c
hoz-code <hozkar50@gamil.com>
2025-10-12 17:19:53 -0500

    after set html file and server the static page web

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 //First Line
-import express from 'express'
+
+import path from 'path' //module inside nodejs
+import { fileURLToPath } from 'url'; //module inside nodejs
+
+import express from 'express' //module installed 
+
+
+
 const app = express();
 const port = 8080 || process.env.PORT
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+app.use(express.static(path.join(__dirname, 'frontend')))
 
 app.get('/', (req, res) => {
     res.send({ "response": "Ok response" })
