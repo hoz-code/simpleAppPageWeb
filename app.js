8973ef34d86344e027bfb245d04bfb9378e1df69
hoz-code <hozkar50@gamil.com>
2025-10-12 16:43:12 -0500

    after set the server response

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //First Line
+import express from 'express'
+const app = express();
+const port = 8080 || process.env.PORT
+
+
+app.get('/', (req, res) => {
+    res.send({ "response": "Ok response" })
+})
+
+app.listen(port, () => { console.log(`Server is listening: Port ${port}`) })
