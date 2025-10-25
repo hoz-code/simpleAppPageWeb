77fc6657e6aaed8e8b6e3bee585ed2435a08f116
hoz-code <hozkar50@gamil.com>
2025-10-24 20:18:33 -0500

    Changes for render host

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ import exphbs from 'express-handlebars'; //module installed
 
 const app = express();
 //const port = 3000 || process.env.PORT
-const port = 3000
+const port = process.env.PORT || 4000
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, 'frontend')))
 //app.use(express.static(path.join(__dirname, 'views')))
 
-console.log('THIS LOG IS PROBE FOR FLY SERVICE')
+console.log('THIS LOG IS PROBE FOR RENDER SERVICE')
 
 app.engine('handlebars', exphbs.engine({
     defaultLayout: 'main',
@@ -28,12 +28,12 @@ app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 
-app.get('/hbs', (req, res) => {
+/*app.get('/hbs', (req, res) => {
     res.render('home', {
         title: 'Welcome to Handlebars!',
         message: 'This is a dynamic message from Express.'
     })
-})
+})*/
 
 
 app.get('/', (req, res) => {
@@ -42,7 +42,8 @@ app.get('/', (req, res) => {
 
 //app.listen(port, () => { console.log(`Server is listening: Port ${port}`) }) //This line is for set the listener server in local host or in the same net
 
-app.listen(port, '127.0.0.1', () => { console.log(`Server is listening: Port ${port}`) }) //This line is for set the listener server in local host or in the same net
+//app.listen(port, '127.0.0.1', () => { console.log(`Server is listening: Port ${port}`) }) //This line is for set the listener server in local host or in the same net
 
+app.listen(port, '127.0.0.1', () => { console.log(`Server is listening: Port ${port}`) }) //This line is for set the listener server in local host or in the same net
 
 //app.listen(port, '0.0.0.0',() => { console.log(`Server is listening: Port ${port}`) }) //This line is for set the listener server in internet
