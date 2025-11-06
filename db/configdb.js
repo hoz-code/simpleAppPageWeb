cbf33ff0fbeae219a6076e000d6ac2577958a2c7
hoz-code <hozkar50@gamil.com>
2025-11-06 17:15:03 -0500

    Config conexion to postgresql server

---
+++ db/configdb.js
@@ -0,0 +1,79 @@
+//import pg from 'pg'
+import { Client } from 'pg'
+
+const database = async () => {
+
+    const client = new Client()
+    await client.connect()
+
+    try {
+        const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+        console.log(res.rows[0].message) // Hello world!
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.end()
+    }
+
+}
+
+
+
+
+
+/*
+const database = async () => {
+    const {Client} = pg
+    console.log('Im function database')
+    const client = new Client();
+
+    await client.connect()
+
+    console.log('Im POSTGRES funciton')
+
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message)
+    await client.end()
+
+};
+*/
+/*
+const database = async () => {
+    const { Client } = pg
+
+    const client = new Client({
+        host: '127.0.0.1', // use IPv4 to avoid ::1 issues
+        port: 5432,
+        user: 'your_db_user',
+        password: 'your_db_password',
+        database: 'your_db_name'
+    });
+    try {
+        await client.connect()
+        const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+        console.log(res.rows[0].message)
+    } catch (err) {
+        console.error('Connection error:', err);
+    } finally {
+        await client.end()
+    }
+
+
+
+
+};
+
+const setPool = () => {
+    const { pool } = new Pool(
+        {
+            user: 'user',
+            host: 'localhost',
+            database: 'databasename',
+            password: 'password',
+            port: 5432
+        }
+    )
+}
+database();
+*/
+export { database };
